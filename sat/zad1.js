116efca5e2ffca4530d22d560579d245a590f8b3
MatejaRadivojevic <93093451+MatejaRadivojevic@users.noreply.github.com>
2022-12-21 20:36:39 +0100

    Delete zad1.js

--- sat/zad1.js
+++
@@ -1,7 +0,0 @@
-let dugme=document.getElementById('dugme');
-dugme.addEventListener('mouseover', function(){dugme.style.backgroundColor='red'});
-dugme.addEventListener('mouseout', function(){dugme.style.backgroundColor='green'});
-function idi(){
-    window.location.href='zad1-1.html'
-}
-dugme.addEventListener('click',idi);
